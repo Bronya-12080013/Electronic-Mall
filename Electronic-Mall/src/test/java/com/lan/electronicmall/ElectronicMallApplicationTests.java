0c1e600e7f43ee4f2cf7396f0f78e8ad23277984
Bronya-12080013 <84848688+Bronya-12080013@users.noreply.github.com>
2022-06-30 09:29:41 +0800

    Add files via upload

---
+++ Electronic-Mall/src/test/java/com/lan/electronicmall/ElectronicMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lan.electronicmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
